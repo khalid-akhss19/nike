d4f5d72d358ef47e6b259fc2a73d3795c18844df
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2023-09-20 16:42:39 +0500

    import seprate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,43 @@
+import {
+  Hero,
+  Popular,
+  SuperQuality,
+  Service,
+  SpecialOffer,
+  CustomerReviews,
+  Subscribe,
+  Footer
+
+} from './sections'
+import Nav from './components/Nav'
+
 export default function App() {
   return (
     <main className="relative">
-      Nav
+      <Nav/>
       <section className="xl:padding-1 wide:padding-r padding-b">
-        Hero
+        < Hero/>
       </section>
       <section className="padding">
-        Popular Products 
+        <Popular/> 
       </section>
       <section className="padding">
-        SuperQuality
+        <SuperQuality/>
       </section>
       <section className="padding-x padding-y">
-        Services
+        <Service/>
+      </section>
+      <section className="padding-x padding-y">
+        <SpecialOffer/>
       </section>
       <section className="padding bg-pale-blue">
-        Customer Reviews
+        <CustomerReviews/>
       </section>
       <section className="padding-x sm:py-32 py-16 w-full">
-        Subscribe
+        <Subscribe/>
       </section>
       <section className="bg-black padding-x padding-t pb-8">
-        Foooter
+        <Footer/>
       </section>
     </main>
   )
---
+++ src/sections/index.js
@@ -0,0 +1,20 @@
+import Hero from "./Hero";
+import Footer from "./Footer"
+import Service from "./Services"
+import SpecialOffer from "./SpecialOffer"
+import Popular from "./Popular"
+import Subscribe from "./Subscribe"
+import SuperQuality from "./SuperQuality"
+import CustomerReviews from "./CustomerReviews"
+
+
+export {
+ Hero,
+ Footer,
+ Service,
+ SpecialOffer,
+ Popular,
+ Subscribe,
+ SuperQuality,
+ CustomerReviews
+}
