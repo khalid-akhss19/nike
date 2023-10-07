556209274421387d7adbb99b91160e10c95f49d3
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2023-10-07 21:43:32 +0500

    header and hero page

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import {headerLogo} from "../assets/images" //we have name expor so we will import like {headerLogo}
+import ham from "../assets/icons/hamburger.svg"
+import { navLinks } from '../constants'
 
 const Nav = () => {
   return (
-    <div>Nav bar here</div>
+    <>
+     <header className="padding-x py-8 absolute z-10 w-full" >
+      <nav className="flex justify-between items-center max-container">
+        <a href='/'>
+          <img src={headerLogo} alt="logo " width={130} height={29} />
+        </a>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-lg:hidden">
+          {
+            navLinks.map((item)=>(
+              <li key={item.label}>
+                <a href={item.href} className='font-montserrat leading-normal text-lg text-slate-gray'>
+                  {item.label}
+                </a>
+              </li>
+            ))
+          }
+        </ul>
+        <div className=' hidden max-lg:block'>
+            <img src={ham} alt='hamburger' width={25} height={25}/>
+          </div>
+        
+      </nav>
+      
+     </header>
+     </>
   )
 }
 
