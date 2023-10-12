4e46b9cf0294c3a2c91b258dd9be215094d7c209
khalid-akhss19 <khalid.ghulbi22@gmail.com>
2023-10-12 21:19:20 +0500

    all

--- src/sections/CustomerReviews.jsx
+++ src/sections/CustomerReviews.jsx
@@ -1,6 +1,30 @@
+import ReviewsCard from "../components/ReviewsCard"
+import {reviews} from "../constants"
 const CustomerReviews = () => {
   return (
-    <div>CustomerReviews</div>
+    <section className="max-container">
+      <h3 className="palanquin text-center text-4xl font-bold">
+        What Our
+        <span className="text-coral-red"> Customer </span>
+        Say?
+      </h3>
+      <p className="info-text m-auto mt-4 max-w-lg text-center">
+      Hear genuine sstories form our satisfird customers about their exceptional experience awith us.
+      </p>
+      <div className="mt-24 flex flex-1 justify-evenly items-center max-lg:flex-col gap-14">
+        {
+          reviews.map((review)=>(
+            <ReviewsCard
+            key={review.id}
+            imgURL={review.imgURL}
+            customerName={review.customerName}
+            rating={review.rating}
+            feedback={review.feedback}
+            />
+          ))
+        }
+      </div>
+    </section>
   )
 }
 
